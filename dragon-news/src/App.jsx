1ab75ed480ce7e2011a669c507bda83b1f72effe
nazmul5675 <hridoyn61@gmail.com>
2025-10-19 10:18:52 +0600

    first commits

---
+++ dragon-news/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='btn text-xl bg-red-100 text-red-500'>this is btn</h1>
+    </>
+  )
+}
+
+export default App
---
+++ dragon-news/src/Routes/Router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from 'react-router';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1 className='btn text-xl bg-red-100 text-red-500'>this is btn</h1>,
+    },
+]);
+
+
+
+export default router;
